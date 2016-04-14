a559739481565326ff482e95cdb9dce253b9ac67
Bruno Ricardo <brunoricardojava@gmail.com>
2016-04-14 18:07:29 -0300

    Implementação da tela de detalhes dos hectares

--- www/js/services.js
+++ www/js/services.js
@@ -9,27 +9,42 @@ angular.module('starter.services', [])
     name: 'Hectare 1',
     lastText: 'Working',
     qntL: 25, //Variável contendo a quantidade de Litros utilizada pelo Hectare
+    qntSensor: 9, //Contem o numero de sensores ativos no hectare selecionado
+    lastIrrigation: '10/04/2016', //Contem a data da ultima irrigação feita no hectare selecionado
+    nextIrrigation: '28/04/2016', //Contem a data da proxima irrigação a ser feita no hectare selecionado
 
   }, {
     id: 1,
     name: 'Hectare 2',
     lastText: 'Working',
     qntL: 40,
+    qntSensor: 9, //Contem o numero de sensores ativos no hectare selecionado
+    lastIrrigation: '10/04/2016', //Contem a data da ultima irrigação feita no hectare selecionado
+    nextIrrigation: '28/04/2016', //Contem a data da proxima irrigação a ser feita no hectare selecionado
   }, {
     id: 2,
     name: 'Hectare 3',
     lastText: 'Error',
     qntL: 5,
+    qntSensor: 9, //Contem o numero de sensores ativos no hectare selecionado
+    lastIrrigation: '10/04/2016', //Contem a data da ultima irrigação feita no hectare selecionado
+    nextIrrigation: '28/04/2016', //Contem a data da proxima irrigação a ser feita no hectare selecionado
   }, {
     id: 3,
     name: 'Hectare 4',
     lastText: 'Working',
     qntL: 90,
+    qntSensor: 9, //Contem o numero de sensores ativos no hectare selecionado
+    lastIrrigation: '10/04/2016', //Contem a data da ultima irrigação feita no hectare selecionado
+    nextIrrigation: '28/04/2016', //Contem a data da proxima irrigação a ser feita no hectare selecionado
   }, {
     id: 4,
     name: 'Hectare 5',
     lastText: 'Error',
     qntL: 2,
+    qntSensor: 9, //Contem o numero de sensores ativos no hectare selecionado
+    lastIrrigation: '10/04/2016', //Contem a data da ultima irrigação feita no hectare selecionado
+    nextIrrigation: '28/04/2016', //Contem a data da proxima irrigação a ser feita no hectare selecionado
   }];
 
   return {
