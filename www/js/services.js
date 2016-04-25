4652351794dca843d08c18f0bbe7d85d2e788942
Bruno Scherer <brunoricardojava@gmail.com>
2016-04-25 15:51:00 -0300

    Adicionado as unidade de medida dos sensores

--- www/js/services.js
+++ www/js/services.js
@@ -66,9 +66,6 @@ angular.module('starter.services', [])
     all: function() {
       return chats;
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
     get: function(chatId) {
       for (var i = 0; i < chats.length; i++) {
         if (chats[i].id === parseInt(chatId)) {
